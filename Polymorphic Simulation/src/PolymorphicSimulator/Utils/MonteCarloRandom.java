6c60fc51b2c237e8aa5e5929585e9897d620e87e
naafidasana <ibrahimnaafi@gmail.com>
2024-10-22 20:05:04 +0200

    Add monte carlo random number generator

---
+++ Polymorphic Simulation/src/PolymorphicSimulator/Utils/MonteCarloRandom.java
@@ -0,0 +1,9 @@
+package PolymorphicSimulator.Utils;
+
+import java.util.Random;
+import java.util.stream.DoubleStream;
+public class Random {
+    private Random Random;
+
+    public MonteCarloRandom() {}
+}
